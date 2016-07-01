0021c57bcbbd77335c017d2ae2c6fd9f5310693a
Fernando Nesi <fernando.nesi@cresolsicoper.com.br>
2016-07-01 16:49:55 -0300

    publicacao

---
+++ sql/06 - Consulta juros compostos (final).sql
@@ -0,0 +1,72 @@
+﻿with
+
+parametros as (
+  select 1000 as montante_inicial,
+         100 as deposito_mensal,
+         '2016/07/01'::date as dt_inicial,
+         '2019/12/31'::date dt_final,
+         12 as taxa_aa,
+         (12/360.0)/100 as taxa_juros_dia
+),
+
+periodos as (
+  select montante_inicial,
+         deposito_mensal,
+         taxa_juros_dia,
+         (-1 + dense_rank() over(order by to_char(periodo_dia::date,'yyyy/MM'))) as num_mes,
+         row_number() over() as periodo,
+         periodo_dia
+    from parametros, (select generate_series(dt_inicial, dt_final, '1 day')::date as periodo_dia from parametros) as generate
+),
+
+calcula_juros As (
+  SELECT montante_inicial,
+         taxa_juros_dia,
+         num_mes,
+         periodo,
+         periodo_dia,
+         (montante_inicial * POWER((1 + taxa_juros_dia),periodo)) - (montante_inicial * POWER((1 + taxa_juros_dia),periodo - 1)) As rendimento_sem_deposito,
+         (montante_inicial * POWER((1 + taxa_juros_dia),periodo)) As montante_total_sem_deposito,
+         (((deposito_mensal*num_mes) + montante_inicial) * POWER((1 + taxa_juros_dia),periodo)) - (((deposito_mensal*num_mes) + montante_inicial) * POWER((1 + taxa_juros_dia),periodo - 1)) As rendimento_com_deposito,
+         (((deposito_mensal*num_mes) + montante_inicial) * POWER((1 + taxa_juros_dia),periodo)) As montante_total_com_deposito,
+         ((deposito_mensal*num_mes) + montante_inicial) as valor_investido,
+         deposito_mensal
+FROM periodos
+),
+
+
+resultado as (
+  select periodo_dia,
+         taxa_juros_dia,
+         montante_inicial,
+         montante_total_sem_deposito,
+         montante_total_com_deposito,
+         rendimento_sem_deposito,
+         rendimento_com_deposito,
+         valor_investido,
+         deposito_mensal,
+         (SELECT sum(rendimento_sem_deposito)
+            FROM calcula_juros As tint
+           WHERE tint.periodo <= tout.periodo
+         ) As rendimento_acumulado_sem_deposito,
+         (SELECT sum(rendimento_com_deposito)
+            FROM calcula_juros As tint
+           WHERE tint.periodo <= tout.periodo
+         ) As rendimento_acumulado_com_deposito
+    from calcula_juros as  tout
+)
+
+
+
+(select periodo_dia,
+       'Total sem depositos'::text as composto,
+       montante_total_sem_deposito
+  from resultado
+)
+union all
+(select periodo_dia,
+       'Total com depositos'::text as composto,
+       rendimento_com_deposito
+  from resultado
+)
+order by composto, periodo_dia
---
+++ sql/06 - Consulta juros compostos.sql
@@ -0,0 +1,46 @@
+﻿with
+
+entradas as (
+  select 1000 as montante,
+         12 as taxa_aa,
+         0.12/360 as taxa_juros
+),
+
+periodos as (
+  select montante,
+         taxa_juros,
+         row_number() over() as periodo,
+         periodo_dia
+         from entradas
+         cross join (select generate_series('01/01/2016'::date, -- dt_compra
+                                      '31/01/2019'::date, -- dt_vencimento
+                                      '1 day')::date as periodo_dia
+                                      ) as pd
+),
+
+calcula_juros As (
+  SELECT montante, taxa_juros,
+         (montante * POWER((1 + taxa_juros),periodo)) - (montante * POWER((1 + taxa_juros),periodo - 1)) As rendimento,
+         (montante * POWER((1 + taxa_juros),periodo)) As total,
+         periodo,
+         periodo_dia
+FROM periodos
+),
+
+resultado as (
+  select periodo_dia,
+         montante as montante_inicial,
+         total as montante_total,
+         taxa_juros,
+         rendimento,
+         (SELECT sum(rendimento)
+            FROM calcula_juros As tint
+           WHERE tint.periodo <= tout.periodo
+         ) As rendimento_acumulado
+    from calcula_juros as  tout
+)
+
+select periodo_dia,
+       montante_total
+  from resultado
+order by periodo_dia
